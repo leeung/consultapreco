bb62672a3e83dbf315f2546aed486bba77c64792
leeung <leeungalves@gmil.com>
2020-09-29 17:56:02 -0300

    Alteracao funcionalidade coletagem

--- www/js/init.js
+++ www/js/init.js
@@ -1,19 +1,20 @@
 console.log('-----Declarando constantes----');
 
-const versao 			= '20200902_1';
+const versao 			= '20200902_2';
 const servidor 			= 'http://192.168.0.200/srvbuscapreco/';
 
 const getConsulta 		= servidor + "index.php/consulta/consultaproduto/";
 const getIpCliente 		= servidor + "index.php/consulta/getIpCliente/";
 const urlFoto			= servidor + "assets/imagens_produtos/";
 const urlBanner			= servidor + "assets/imagens_banner/";
-const instalador 		= 'http://192.168.0.200/arquivos/apk'+versao+".apk";
+const instalador 		= 'http://192.168.0.200/srvbuscapreco/assets/instalador/'+versao+".apk";
 
 (function($){
   $(function(){
   	$("#versao").text(".. V:"+versao+"..");
     $('.sidenav').sidenav();
     setInterval(function(){ getIp(); }, 5000);
+	$('#icoDownload').attr('href',instalador);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
