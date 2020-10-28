8d123d854ae24e29c46f03bb1489b127406f277f
leeung <leeungalves@gmil.com>
2020-10-28 15:38:24 -0300

    Versao 20201028

--- www/js/funcoes.js
+++ www/js/funcoes.js
@@ -1,27 +1,98 @@
- //funções especificas 
-//var endSrvIp = 'http://192.168.0.200/buscapreco2/index.php/Consulta/getipCliente';
-//var endSrvIp = 'http://192.168.0.125/projetos/casafreitas/buscapreco2/index.php/Consulta/getipCliente';
 
- var getIp = function() {
+//FECHA TELA DE CONSULTA
+function fecharModal(){
+  clearTimeout(objTimeOutModal);
+  objTimeOutModal = setTimeout(function(){ $('.modal').modal('close'); }, timeOutModal);
+  $('#ean').val("");
+}
 
+//PEGA IP DO CLIENT
+var getIp = function() {
  	var ip = '';
-    $.getJSON(getIpCliente,
-      function(json) {
-      	ip = json.ip;
-        setInfoIp(ip);
-      }
-    );  
-    setInfoIp(ip);  
-  }
+  $.getJSON(getIpCliente,
+    function(json) {
+      ip = json.ip;
+      setInfoIp(ip);
+    }
+  );  
+  setInfoIp(ip);  
+}
 
-  var setInfoIp = function(ip){
-	if(ip == ''){
-    	log("desconectado "+ip);
+//SETA IP NA TELA
+var setInfoIp = function(ip){
+  if(ip == ''){
+    log("desconectado "+ip);
 		$('#ip').text('IP: sem conexao');
 		$('#ip').addClass('red-text text-lighten-3');
-    }else{
+  }else{
     	log("conectado ip "+ip);
     	$('#ip').text('IP:'+ip);
     	$('#ip').removeClass('red-text text-lighten-3');
-    }
   }
+}
+
+//CAPTURA AS TECLAS
+function capturaTecla(event){
+  $('.modal').modal('close');       
+  var numeros = [1,2,3,4,5,6,7,8,9,0];
+  //log("Tecla digitada: " + event.keyCode);
+  var tecla = event.keyCode - 48;
+  if (numeros.indexOf(tecla) != -1){
+    ean = ean + String(tecla);
+    $('#ean').val(ean);
+    if(ean.length == 13){
+      //log("Executando função de consulta para: " +ean);
+      consultaProduto(ean);
+      ean = "";
+    }
+  }                    
+}
+
+//EXIB LOGS
+function log(msg){
+  console.log(msg);
+  if(gravaLog)
+    $('#telaLog').append('<p class="lime-text text-darken-1">'+msg+'</p>');
+}
+
+  function consultaProduto(codBar){
+    log("Consultando produto: " + codBar);
+    $.ajax({
+    url: getConsulta+codBar+"/null/"+versao,
+    type: "get",
+    dataType: "json"
+    }).done(function(resposta) {
+        console.log(JSON.stringify(resposta));
+        log('return : '+ JSON.stringify(resposta));
+
+        if(resposta.error == false){
+            $('#imgProduto').attr('src','');
+            $('#barra').html('');
+            $('#codigo').html('');
+            $('#descricao').html('');
+            $('#varejo').html('');
+            $('#atacado').html('');
+            $('#quantidade').html('');
+            $('.modal').modal('close');
+
+            $('#imgProduto').attr('src',urlFoto+resposta.result.IMG);
+            $('#barra').html(resposta.result.EAN);
+            $('#codigo').html(resposta.result.CODIGO);
+            $('#descricao').html(resposta.result.DESCRICAO);
+            $('#varejo').html(resposta.result.VAREJO);
+            $('#atacado').html(resposta.result.ATACADO);
+            $('#quantidade').html(resposta.result.MENSAGEM);
+            $('.modal').modal('open');
+            
+        }else if(resposta.error == true){
+            $('#ean').val(resposta.message);
+        }
+        
+    }).fail(function(jqXHR, textStatus ) {
+        log("Falhou na requisição: " + textStatus);
+        $('#ean').val("ERRO DE CONEXAO");
+    }).always(function() {
+        log("Concluido");
+    });
+    //$('#ean').val(codBar);
+}
--- www/js/init.js
+++ www/js/init.js
@@ -1,22 +1,60 @@
 console.log('-----Declarando constantes----');
 
-const versao 			= '20200902_3';
+const versao 			= '20201028';
 const servidor 			= 'http://192.168.0.200/srvbuscapreco/';
 
-const getConsulta 		= servidor + "index.php/consulta/consultaproduto/";
+const getConsulta 		= servidor + "index.php/consulta/produto/";
 const getIpCliente 		= servidor + "index.php/consulta/getIpCliente/";
 const urlFoto			= servidor + "assets/imagens_produtos/";
 const urlBanner			= servidor + "assets/imagens_banner/";
 const instalador 		= 'http://192.168.0.200/srvbuscapreco/assets/instalador/'+versao+".apk";
 
 var gravaLog  = false;
+var ean = "";
+var timeOutModal = 5000;
+var objTimeOutModal = null;
 
 (function($){
   $(function(){
+
   	$("#versao").text(".. V:"+versao+"..");
     $('.sidenav').sidenav();
-    setInterval(function(){ getIp(); }, 5000);
-	$('#icoDownload').attr('href',instalador);
+    $('#icoDownload').attr('href',instalador);
+    $('select').formSelect();
+    $('.tooltipped').tooltip();
+    $('.modal').modal({ inDuration: 30, 
+                        outDuration: 30,
+                        startingTop: '30%',
+                        onOpenEnd: function(){
+                          fecharModal();
+                        }
+                        });
+    $('.slider').slider({
+      indicators: false,
+      height: 400
+    });
 
+    $('#btnHome').floatingActionButton({
+      direction:'top',
+      hoverEnabled: false
+    });
+
+    //HABILIDA GRAVAÇÃO DE LOGS
+    $("#icoLog").click(function(){
+      gravaLog = !gravaLog;
+      if(gravaLog){
+        $("#telaLog").css("height","200px");
+        log("Iniciando gravação de log");
+      }else{
+        $("#telaLog").css("height","0px");  
+        $('#telaLog').html("");
+      }
+    });
+
+    //SETA INTERVALO DA TELA DE CONSULTA
+    setInterval(function(){ getIp(); }, 5000);
+
+    //INCLUI O EVENT KEYPRESS
+    document.addEventListener('keypress', capturaTecla, true);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
